7db3b02f24c7a75cc272a3690ab534d2ff243ec8
Michael Rhey Palaganas <70465004+SeeFlushFlush64@users.noreply.github.com>
2025-11-07 05:18:59 +0800

    add description ref

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,17 @@
 import { ref } from 'vue'
 
 const product = ref('Socks')
+const description = ref('Comfortable cotton socks for everyday wear')
   
 
 setTimeout(() => {
   product.value = "New Socks"
-}, 1000)
+  description.value = "Socks so nice, you'll want to buy them twice"
+}, 4000)
 
 </script>
 
 <template>
   <h1>{{ product }}</h1>
+  <p>{{ description }}</p>
 </template>
